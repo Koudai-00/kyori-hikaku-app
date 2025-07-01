720bc655d7e72c03cf5ddbcdde4fc2ff30fd4d5a
kk19970124 <42162313-kk19970124@users.noreply.replit.com>
2025-07-01 09:14:24 +0000

    Restored to 'a3082ff0674df490aa1616f05edbf13d68a0c103'
    Replit-Restored-To: a3082ff0674df490aa1616f05edbf13d68a0c103

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import { insertUserUsageSchema, insertDistanceQuerySchema, insertArticleSchema, insertContactSchema } from "@shared/schema";
+import { insertUserUsageSchema, insertDistanceQuerySchema, insertArticleSchema } from "@shared/schema";
 import { z } from "zod";
 import multer from "multer";
 import path from "path";
